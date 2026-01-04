de89cddc4ce9523c48b943f0a32f318bbff90275
Ahsan <ahsankhn0987@gmail.com>
2026-01-04 20:24:50 +0500

    Api

--- Frontend/src/Comp/api/Api.js
+++ Frontend/src/Comp/api/Api.js
@@ -12,7 +12,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://daily-pantry.onrender.com/api/v1",
   headers: token ? { Authorization: `Bearer ${token}` } : {},
   withCredentials: true,
 });
